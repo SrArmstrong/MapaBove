93d01697fca5c26f077141e40d7c885e7f04160a
SrArmstrong <cetisaldavalde@gmail.com>
2025-11-29 12:50:25 -0600

    Ev23_1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./index");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
   console.log("Servidor iniciado en puerto", PORT);
